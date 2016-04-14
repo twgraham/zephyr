a9941c16b3851de246cbed09cd75049d5d5010b4
Taylor Graham <taylor.w.graham@gmail.com>
2016-04-14 21:21:45 +1000

    Added search!!

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,8 +13,33 @@
 
         //$("#blog-logo").prependTo("#site-head-content");
         $("div.masthead").autoHidingNavbar();
+        $('.scrollbar-outer').scrollbar();
+        
+        $("#search-field").ghostHunter({
+            results: "#results",
+            onKeyUp: true
+        });
 
     });
 
 }(jQuery));
 
+
+$(function () {
+    $('a[href="#search"]').on('click', function(event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    
+    $('#search, #search button.close').on('click keyup', function(event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
+    
+    $('form').submit(function(event) {
+        event.preventDefault();
+        return false;
+    })
+});
